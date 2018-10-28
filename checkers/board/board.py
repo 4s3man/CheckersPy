24cba7f5cddf26c984c1909d1b021f321a9a537b
Kuba Kułaga <kuba.kulaga.sv7@gmail.com>
2018-10-28 13:02:01 +0100

    board have state now

--- checkers/board/board.py
+++ checkers/board/board.py
@@ -4,11 +4,11 @@ from checkers.board.state import *
 class Board():
     board_size = 8
     pawns_for_site = 12
+    state = None
     def __init__(self, state: State = InitialState()):
-        self.white_pawns = state.white_pawns
-        self.black_pawns = state.black_pawns
+        self.state = state
         self.fields = [[None for x in range(self.board_size)] for y in range(self.board_size)]
-        for pawn in self.black_pawns:
-            self.fields[pawn.y][pawn.x] = self.black_pawns[pawn.id]
-        for pawn in self.white_pawns:
-            self.fields[pawn.y][pawn.x] = self.white_pawns[pawn.id]
+        for pawn in state.black_pawns:
+            self.fields[pawn.y][pawn.x] = state.black_pawns[pawn.id]
+        for pawn in state.white_pawns:
+            self.fields[pawn.y][pawn.x] = state.white_pawns[pawn.id]
