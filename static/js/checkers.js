91db1e452d04353266dc2e11c90570a29355fa72
Kuba Kułaga <kuba.kulaga.sv7@gmail.com>
2018-10-30 11:44:02 +0100

    react board added

--- static/js/checkers.js
+++ static/js/checkers.js
@@ -1,7 +1,63 @@
-// jQuery(function(){
-//     $().ready(function(){
-//       $.post('/ajax/move', function(data){
-//         alert(data);
-//       })
-//     });
-// });
+import React, {Component} from 'react'
+import classnames from 'classnames'
+
+export default class Checkers extends Component{
+  constructor(props, context){
+    super(props);
+    this.state = {
+      fields: this.createFields(),
+      coin: this.getCoin()
+    }
+  }
+
+  createFields(){
+    let fields = [],
+        id = 0,
+        color = null;
+    for (let y=0;y<8;y++){
+      let row = [];
+      for (let x=0;x<8;x++){
+        id++;
+        color = (x+y)%2 == 0? 'white' : 'black';
+        row = [...row, BoardField(color, id)];
+      }
+      fields = [...fields, row];
+    }
+    return fields;
+  }
+
+  getCoin(){
+    return (
+      <Coin id='1' color='white' moves={[]} handler={this.coinHandler} />
+    )
+  }
+
+  coinHandler(){
+    alert('ok');
+  }
+
+  render(){
+    return (
+      <div className='board'>
+        {this.state.fields}
+      </div>
+    );
+  }
+}
+const BoardField = (color, key, coin=null) => {
+    let blockClass = 'board__field';
+    return (
+      <div
+       className={classnames(blockClass, blockClass+'--'+color)}
+       key={key}
+       >
+       {coin}
+      </div>
+    );
+  }
+const Coin = (id, color, moves, handler) => {
+  let blockClass = 'coin';
+  return (
+    <span className={classnames(blockClass, blockClass+'--'+color)} />
+  )
+}
--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,8 @@
-alert('hello')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Checkers from './checkers'
+
+console.log('dono');
+
+ReactDOM.render(<Checkers/>, document.getElementById('JScheckers'));
