9fbf46da516a892fa91abed3ca660f38b1386b2b
Kuba Kułaga <kuba.kulaga.sv7@gmail.com>
2018-11-03 12:20:04 +0100

    typo fix

--- tests/board_test.py
+++ tests/board_test.py
@@ -1,7 +1,7 @@
 import pytest
 from checkers.board import board as cb
 
-def test_place_pawn_same_coin_on_field_and_state():
+def test_place_pawn_same_pawn_on_field_and_state():
     """Checking on initialState case"""
     board = cb.Board()
     assert board.state.black_pawns[0] is board.fields[0][0]
