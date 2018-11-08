1d7130b014e593fb2c9e0fa05a98ee4c4ae885a9
Kuba Kułaga <kuba.kulaga.sv7@gmail.com>
2018-11-09 00:59:52 +0100

    after get_jump_moves_for_pawn refractorization

--- checkers/board/move_resolver.py
+++ checkers/board/move_resolver.py
@@ -1,7 +1,9 @@
 from checkers.board.board import *
+import copy
 
 class MoveResolver():
     board = None
+    directions = [(1,1), (1,-1), (-1, -1), (-1,1)]
 
     def __init__(self, state: State = None):
         self.board = Board()
@@ -10,7 +12,8 @@ class MoveResolver():
 
     def resolve_moves(self, state: State)->State:
         self.board.place_pawns(state)
-
+        moves = self.get_jump_moves_for_pawn(state.black_pawns[0], [])
+        print(moves)
         # for pawn in state.white_pawns + state.black_pawns:
         #     if pawn: self.resolve_pawn_moves(pawn)
         # """debug to remove"""
@@ -19,33 +22,61 @@ class MoveResolver():
         # """debug to remove"""
         return state
 
-    def resolve_pawn_moves(self, pawn: Pawn):
-        if pawn.type == 'normal':
-            self.get_moves_for_pawn(pawn)
-        # elif pawn.type == 'queen':
-        #     self.get_moves_for_queen(pawn)
+    # def resolve_pawn_moves(self, pawn: Pawn):
+    #     if pawn.type == 'normal':
+    #         self.get_moves_for_pawn(pawn)
+    #     # elif pawn.type == 'queen':
+    #     #     self.get_moves_for_queen(pawn)
+    #
+    # def get_moves_for_pawn(self, pawn: Pawn):
+    #     pawn.moves = self.get_jump_moves(pawn) or self.get_pawn_normal_moves(pawn)
+    #     pass
+    #
+    # def get_jump_moves(self, pawn: Pawn):
+    #     pass
+    #
+    # def get_pawn_normal_moves(self, pawn: Pawn):
+    #     pass
 
-    def get_moves_for_pawn(self, pawn: Pawn):
-        pawn.moves = self.get_jump_moves(pawn) or self.get_pawn_normal_moves(pawn)
-        pass
+    def get_jump_moves_for_pawn(self, pawn: Pawn, move_list: list, move: dict={}, debug=0)-> list:
+        """TODO zmienić tak żeby łapało tylko pawn.id do move a nie całe obiekty pawn"""
+        if self.pawn_has_obligatory_move(pawn, move):
+            for y, x in self.directions:
+                try:
+                    pawn_in_direction = self.get_pawn_in_direction(pawn, (y, x))
+                    if self.pawn_can_jump_in_direction(pawn, pawn_in_direction, (y, x), move):
+                        next_y, next_x = pawn_in_direction.y + y, pawn_in_direction.x + x
 
-    def get_jump_moves(self, pawn: Pawn):
-        pass
+                        move_inst = {'pos':[], 'beated_pawn_ids':[]} if not len(move) else copy.deepcopy(move)
+                        move_inst['pos'].append((next_y, next_x))
+                        move_inst['beated_pawn_ids'].append(pawn_in_direction.id)
 
-    def get_pawn_normal_moves(self, pawn: Pawn):
-        pass
+                        pawn1 = copy.deepcopy(pawn)
+                        pawn1.y = next_y
+                        pawn1.x = next_x
 
-    # def have_obligatory_move(self, pawn: Pawn, move: dict):
-    #     for y, x in [(1,1), (1,-1), (-1, 1), (-1,-1)]:
-    #         try:
-    #             pawn_in_direction = self.get_pawn_in_direction(pawn, y, x)
-    #             if self.can_jump_in_direction(pawn, pawn_in_direction, y, x, move): return True
-    #         except BoardError:
-    #             pass
-    #     return False
+                        if move_inst not in move_list\
+                        and not self.pawn_has_obligatory_move(pawn1, move_inst)\
+                        and not self.same_pos_in_movelist(move_inst, move_list):
+                            move_list.append(move_inst)
 
-    """Throws NoCoinError, OutOfBoardError"""
-    def get_pawn_in_direction(self, pawn: Pawn, y:int, x:int):
+                        self.get_jump_moves_for_pawn(pawn1, move_list, move_inst)
+                except BoardError:
+                    pass
+        return move_list
+
+    def pawn_has_obligatory_move(self, pawn: Pawn, move: dict = {})->bool:
+        for y, x in self.directions:
+            try:
+                pawn_to_check = self.get_pawn_in_direction(pawn, (y, x))
+                if self.pawn_can_jump_in_direction(pawn, pawn_to_check, (y, x), move): return True
+            except BoardError:
+                pass
+        return False
+
+    def get_pawn_in_direction(self, pawn: Pawn, direction: tuple)->Pawn:
+        """Throws NoCoinError, OutOfBoardError or gets pawn in direction"""
+        y,x = direction
         x += pawn.x
         y += pawn.y
         if not self.board.has_position(y, x): raise OutOfBoardError('Field in direction out of the board')
@@ -53,9 +84,25 @@ class MoveResolver():
         if pawn_in_direction is None: raise NoCoinError('No coin in this direction')
         return pawn_in_direction
 
-    # def search_enemy_in_direction(self, pawn:Pawn, y:int, x:int):
-    #     board = self.board
-    #     if not board.has_position(y, x): raise OutOfBoardError('Field in direction out of the board')
-    #     coin_in_direction = board.fields[y][x]
-    #     if coin_in_direction is None: raise NoCoinError('No coin in this direction')
-    #     return coin_in_direction
+    def pawn_can_jump_in_direction(self, this_pawn: Pawn, pawn_to_check: Pawn, vector: tuple, move: dict):
+        """false if position after jump is out of board"""
+        y, x = (v*2 for v in vector)
+        if not self.board.has_position(this_pawn.y + y, this_pawn.x + x): return False
+
+        """false if pawns have same colors"""
+        if pawn_to_check.color == this_pawn.color: return False
+
+        """false if on field after jump is pawn and it's not the pawn who jumps"""
+        field_after_jump = self.board.fields[this_pawn.y + y][this_pawn.x + x]
+        if isinstance(field_after_jump, Pawn)\
+        and field_after_jump is not this_pawn: return False
+
+        """if move is calculating and coin_in_direction wasn't beated yet"""
+        if len(move) and (pawn_to_check.id not in move['beated_pawn_ids']): return False
+
+        return True
+
+    def same_pos_in_movelist(self, move: dict, move_list: list)-> bool:
+        for move_l in move_list:
+            if set(move_l['pos']) == set(move['pos']): return True
+        return False
--- checkers/tests/move_resolver_test.py
+++ checkers/tests/move_resolver_test.py
@@ -7,14 +7,26 @@ def test_get_pawn_in_direction(different_coins_around_white):
     border_pawn = state.black_pawns[3]
     move_res = MoveResolver(state)
 
-    assert state.black_pawns[0] is move_res.get_pawn_in_direction(pawn, -1, -1)
-    assert state.black_pawns[0] is not move_res.get_pawn_in_direction(pawn, -1, 1)
+    assert state.black_pawns[0] is move_res.get_pawn_in_direction(pawn, (-1, -1))
+    assert state.black_pawns[0] is not move_res.get_pawn_in_direction(pawn, (-1, 1))
 
-    assert state.white_pawns[2] is move_res.get_pawn_in_direction(pawn, -1, 1)
-    assert state.white_pawns[1] is not move_res.get_pawn_in_direction(pawn, -1, 1)
+    assert state.white_pawns[2] is move_res.get_pawn_in_direction(pawn, (-1, 1))
+    assert state.white_pawns[1] is not move_res.get_pawn_in_direction(pawn, (-1, 1))
 
     with pytest.raises(NoCoinError):
-        assert move_res.get_pawn_in_direction(pawn, 1, 1)
+        assert move_res.get_pawn_in_direction(pawn, (1, 1))
 
     with pytest.raises(OutOfBoardError):
-        assert move_res.get_pawn_in_direction(border_pawn, -1, 1)
+        assert move_res.get_pawn_in_direction(border_pawn, (-1, 1))
+
+def test_pawn_has_obligtory_move(different_coins_around_white):
+    state = different_coins_around_white
+    move_res = MoveResolver(state)
+
+    assert True == move_res.pawn_has_obligatory_move(state.white_pawns[1])
+    assert False == move_res.pawn_has_obligatory_move(state.white_pawns[2])
+
+    assert True == move_res.pawn_has_obligatory_move(state.black_pawns[0])
+    assert False == move_res.pawn_has_obligatory_move(state.black_pawns[1])
+    assert False == move_res.pawn_has_obligatory_move(state.black_pawns[2])
+    assert False == move_res.pawn_has_obligatory_move(state.black_pawns[3])
