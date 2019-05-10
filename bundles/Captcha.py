7595c0c76c75bfbcaae03a211338fd11e0776046
Jakub Kułaga <kuba.kulaga.sv7@gmail.com>
2019-05-10 09:32:50 +0200

    captcha changed

--- bundles/Captcha.py
+++ bundles/Captcha.py
@@ -3,7 +3,7 @@ from ast import literal_eval
 import json
 
 def captcha_is_ok(request):
-    secret_key = "6LfQTqIUAAAAANVt8PZn2kP5Oqz99mk8mt8zITWh"
+    secret_key = ""
     captcha = request.form.get('g-recaptcha-response')
     server_ip = request.host.split(':')[0]
     response = urllib.request.urlopen("https://www.google.com/recaptcha/api/siteverify?secret=" + secret_key + "&response=" + captcha + "&remoteip=" + server_ip).read(1000)
